ff295cbc7e7ed6731c0eb5ecd89e660e53994e31
Ankit470 <ankitomega@gmail.com>
2021-04-28 16:49:41 +0530

    commit

--- ecommerce/ecomm/shop/views.py
+++ ecommerce/ecomm/shop/views.py
@@ -3,3 +3,22 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("we're at about")
+
+def contact(request):
+    return HttpResponse("we're at contact")
+
+def tracker(request):
+    return HttpResponse("we're at tracker")
+
+def search(request):
+    return HttpResponse("we're at search")
+
+def productView(request):
+    return HttpResponse("we're at product view")
+
+def checkout(request):
+    return HttpResponse("we're at checkout")
+
