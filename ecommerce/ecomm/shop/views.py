56e949c487aa18ff762b468b7702f561e23adcd1
Ankit470 <ankitomega@gmail.com>
2021-07-09 02:18:58 +0530

    commit

--- ecommerce/ecomm/shop/views.py
+++ ecommerce/ecomm/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 def index(request):
@@ -28,17 +28,29 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
-    return HttpResponse("we're at contact")
+    if request.method=="POST":
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        print(name,email ,phone ,desc)
+        contact = Contact(name = name , email = email ,phone = phone ,desc = desc )
+        contact.save()
+    return render(request,'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("we're at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("we're at search")
+    return render(request,'shop/search.html')
 
-def productView(request):
-    return HttpResponse("we're at product view")
+def productView(request,myid):
+    #fetch the product using the id 
+    product = Product.objects.filter(id=myid)
+    print(product)
+    return render(request,'shop/prodview.html',{'product':product[0]})
+    
 
 def checkout(request):
-    return HttpResponse("we're at checkout")
+    return render(request,'shop/checkout.html')
 
