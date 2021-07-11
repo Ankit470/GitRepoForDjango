c4736cdb15e9a3a0c6418379375b98f7d643ccba
Ankit470 <ankitomega@gmail.com>
2021-07-12 04:53:23 +0530

    12th july commit

--- ecommerce/ecomm/shop/admin.py
+++ ecommerce/ecomm/shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contact, Product
+from .models import Contact, Orders, Product
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
