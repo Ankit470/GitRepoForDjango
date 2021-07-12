d905b808b299f527dde975a136953a2625094282
Ankit470 <ankitomega@gmail.com>
2021-07-13 03:55:52 +0530

    13July Commit

--- ecommerce/ecomm/ecomm/settings.py
+++ ecommerce/ecomm/ecomm/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ecomm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ecomm/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
